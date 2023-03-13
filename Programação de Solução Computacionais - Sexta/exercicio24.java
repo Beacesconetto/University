d542a342e959342812dc40b15d87a4f1a532256f
Beacesconetto <you@example.com>
2023-03-13 15:14:34 -0300

    Aula5

---
+++ Programação de Solução Computacionais - Sexta/exercicio24.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class exercicio24 {
+    public static void main(String[] args) {
+        int i = 13;
+
+		  do{
+		    i++;
+		    if(( i % 2 ) == 0 ){
+			   JOptionPane.showMessageDialog(null,"É par: " + i); 
+			}
+		  }while( i < 23 );	
+    }
+    
+}
